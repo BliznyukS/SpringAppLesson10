d2e219159a12d7212a243245f6225935782b42a8
BliznyukS <bliznyuk.sergey@gmail.com>
2020-04-06 14:16:53 +0300

    @Autowired with Field

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -5,16 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
+    @Autowired
     private Music music;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
 
-    @Autowired
-    public void setMusic(Music music) {
-        this.music = music;
-    }
 
 
     public void playMusic() {
